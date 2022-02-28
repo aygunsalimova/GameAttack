51d67bada937dfd79e2572fa5c0bac4db01086a0
Aygun Salimova <79048694+aygunsalimova@users.noreply.github.com>
2022-02-28 16:01:51 +0400

    Add files via upload

---
+++ vue-1-GameAttack/script.js
@@ -0,0 +1,17 @@
+Vue.createApp({
+    data() {
+        return {
+            displayActive: none,
+        }
+
+    },
+
+    computed() {
+        change() {
+
+        }
+    }
+
+}).mount('#app')
+
+console.log(displayActive);
